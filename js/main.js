99aa90ef99d9009a8948c21186a14ffbf6efcc06
Dwi Darmayasa <58174204+DwiDarmayasa@users.noreply.github.com>
2021-06-23 01:10:11 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -217,10 +217,26 @@
 	------------------------------------------------------ */
 
 	/* local validation */
-	$('#contactForm').validate({
+		function encode(data) {
+			return Object.keys(data)
+				.map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+				.join("&")
+		  }
+		
+		const handleSubmit = (event) => {
+		  event.preventDefault()
+		  fetch("/", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: encode({
+			  "contactForm": event.target.getAttribute("name"),
+			  ...name
+			})
+		  }).then(() => navigate("/thank-you/")).catch(error => alert(error))
+		}
 
 		/* submit via ajax */
-		submitHandler: function(form) {
+		/*submitHandler: function(form) {
 
 			var sLoader = $('#submit-loader');
 
@@ -263,6 +279,7 @@
   		}
 
 	});
+	*/ 
 
 
  	/*----------------------------------------------------- */
